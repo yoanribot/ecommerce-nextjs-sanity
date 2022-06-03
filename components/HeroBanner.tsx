360f05d39fe7ecc72d2ff0716531aebe18ec00c1
Yoan <yoanribot@gmail.com>
2022-06-03 10:07:27 +0200

    Create initial Hero Banner component

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
 import React from "react";
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  return (
+    <section className="hero-banner-container">
+      <div>
+        <p className="beats-solo">Small text</p>
+        <h3>Mid text</h3>
+        <img src="" className="hereo-banner-image" alt="headphones" />
+        <div>
+          <Link href={"/product/Id"}>
+            <button type="button"> Button text </button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>Description text</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HeroBanner;
